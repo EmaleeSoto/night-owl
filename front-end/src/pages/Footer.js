836f45b99d27b30fb2deb48d523e6db628d807eb
EmaleeSoto <emaleesoto@gmail.com>
2023-06-13 19:16:43 -0400

    Footer almost entirely complete, colors added to necessary elements, about page cleaned up and redesigned

--- front-end/src/pages/Footer.js
+++ front-end/src/pages/Footer.js
@@ -4,18 +4,32 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <footer class="footer">
-      <div class="footer__content">
-        <div class="footer__logo">
-          {/* <!-- Your logo or brand name here --> */}
-          <h2>Your Brand</h2>
-        </div>
-        <nav class="footer__navigation">
-          <ul class="footer__nav-list">
+      <div className="footer__navigationContainer">
+        <div className="footer__legal">
+          <h5>Legal</h5>
+          <ul>
             <li>
-              <a href="#">Home</a>
+              <a href="#">Privacy</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#">Terms</a>
+            </li>
+            <li>
+              <a href="#">Cookie Policy</a>
+            </li>
+            <li>
+              <a href="#">Intellectual Property</a>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__navList">
+          <h5>Nav</h5>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
             </li>
             <li>
               <a href="#">Services</a>
@@ -24,18 +38,50 @@ export default function Footer() {
               <a href="#">Contact</a>
             </li>
           </ul>
-        </nav>
-        <div class="footer__social-icons">
-          {/* <!-- Add your social media icons here --> */}
-          <a href="#">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-instagram"></i>
-          </a>
+        </div>
+        <div className="footer__social">
+          <h5>Social Media</h5>
+          <ul>
+            <li>
+              <a>
+                <img
+                  src={require("../assets/logos/twitter.png")}
+                  alt="Twitter"
+                />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img
+                  src={require("../assets/logos/instagram.png")}
+                  alt="Instagram"
+                />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src={require("../assets/logos/tiktok.png")} alt="TikTok" />
+              </a>
+            </li>
+            <li>
+              <a>
+                <img
+                  src={require("../assets/logos/facebook.png")}
+                  alt="Facebook"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div class="footer__content">
+        <div class="footer__logo">
+          {/* <!-- Your logo or brand name here --> */}
+          <h2>Get the app!</h2>
+          <div className="logo__mobileAppLogo">
+            <img src={require("../assets/mobile-app.png")} />
+          </div>
         </div>
       </div>
       <div class="footer__bottom">
