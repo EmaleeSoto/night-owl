495c93cf667e74740aa9b6eed6cffb0223fc45fd
EmaleeSoto <emaleesoto@gmail.com>
2023-06-13 01:45:46 -0400

    Added a section in nextPage

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -37,32 +37,45 @@ const Home = () => {
       </div>
       <hr />
       <div className="home__nextPage">
-        <div>
-          <img src={require("../assets/map.png")} alt="Discover new places" />
-          <h3>Discover new places</h3>
-          <p>
-            With Night Owl, you can find new hangout spots that match your
-            preferred vibes.
-          </p>
-        </div>
-        <div>
-          <img
-            src={require("../assets/alcohol.png")}
-            alt="Learn your own tastes"
-          />
-          <h3>Learn your own tastes</h3>
-          <p>
-            Night Owl provides a guide on alcoholic beverages so you can explore
-            new drinks to try on your night out!
-          </p>
-        </div>
-        <div>
-          <img src={require("../assets/dancing.jpeg")} alt="Meet new people" />
-          <h3>Meet new people</h3>
-          <p>
-            What's a good night out without some company? Meet likeminded people
-            to connect with.
-          </p>
+        <section>
+          Night Owl is a platform designed for night owls who love to explore
+          the nightlife scene. Whether you're looking for new hangout spots,
+          want to learn about different alcoholic beverages, or wish to connect
+          with like-minded individuals, Night Owl has got you covered. Join
+          Night Owl and unlock a world of exciting experiences for the nocturnal
+          adventurers
+        </section>
+        <div className="home__nextPage__gridContainer">
+          <div className="home__nextPage__gridCell">
+            <img src={require("../assets/map.png")} alt="Discover new places" />
+            <h3>Discover new places</h3>
+            <p>
+              With Night Owl, you can find new hangout spots that match your
+              preferred vibes.
+            </p>
+          </div>
+          <div className="home__nextPage__gridCell">
+            <img
+              src={require("../assets/alcohol.png")}
+              alt="Learn your own tastes"
+            />
+            <h3>Learn your own tastes</h3>
+            <p>
+              Night Owl provides a guide on alcoholic beverages so you can
+              explore new drinks to try on your night out!
+            </p>
+          </div>
+          <div className="home__nextPage__gridCell">
+            <img
+              src={require("../assets/dancing.jpeg")}
+              alt="Meet new people"
+            />
+            <h3>Meet new people</h3>
+            <p>
+              What's a good night out without some company? Meet likeminded
+              people to connect with.
+            </p>
+          </div>
         </div>
       </div>
     </div>
