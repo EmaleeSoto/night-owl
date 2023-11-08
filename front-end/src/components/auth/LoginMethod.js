e270d1620ebd0230612aacb595c3c6f8a36fa3d0
EmaleeSoto <emaleesoto@gmail.com>
2023-11-08 03:51:28 -0500

    Replaced splashimage with higher res image, changeed color assets, remodeled logindmodal and home first page

--- front-end/src/components/auth/LoginMethod.js
+++ front-end/src/components/auth/LoginMethod.js
@@ -3,9 +3,21 @@ import { Link } from "react-router-dom";
 import "./LoginMethod.scss";
 
 export default function LoginMethod({ closeModal, loginOption, icon }) {
+  function loginMethodBackgroundColor(loginOption) {
+    if (loginOption.includes("Create")) {
+      return "rose";
+    } else {
+      return "default";
+    }
+  }
   return (
     <div className="loginMethod">
-      <button className="loginMethod__loginButton" onClick={closeModal}>
+      <button
+        className={`loginMethod__loginButton loginMethod__${loginMethodBackgroundColor(
+          loginOption
+        )}`}
+        onClick={closeModal}
+      >
         <Link
           className="loginMethod__loginButton__loginLink"
           to={loginOption.includes("Email") ? "/sign-in" : "/sign-up"}
